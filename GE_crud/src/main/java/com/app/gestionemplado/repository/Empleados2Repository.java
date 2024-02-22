999c1e691b88aec844727670e83eff57bf89af84
floryPerez <51434291+floryPerez@users.noreply.github.com>
2024-02-22 10:34:13 -0600

    se elimina la carpeta gecrud
    se elimina carpetas innecesarias

--- GE_crud/src/main/java/com/app/gestionemplado/repository/Empleados2Repository.java
+++
@@ -1,9 +0,0 @@
-package com.app.gestionemplado.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.gestionemplado.entity.empleado2;
-
-public interface Empleados2Repository extends JpaRepository<empleado2, Long> {
-
-}
