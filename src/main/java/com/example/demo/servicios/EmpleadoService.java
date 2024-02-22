4ce4425fd2ebbce0e862a4fbc6c1bfb4ed5220fc
floryPerez <51434291+floryPerez@users.noreply.github.com>
2024-02-22 10:31:51 -0600

    se estandariza el back
    creacion de las carpetas

--- src/main/java/com/example/demo/servicios/EmpleadoService.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.servicios;
-
-public class EmpleadoService {
-
-}
---
+++ src/test/java/com/app/gestionemplado/GeCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.gestionemplado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
